103c612f5e7665a46ea15b512def21c72c3d2113
Swearfy <swearfy@gmail.com>
2023-06-10 19:43:34 +0100

    added active states

--- src/index.js
+++ src/index.js
@@ -11,21 +11,40 @@ const main = document.getElementById("content");
 createHeader();
 loadHomePage(main);
 
-document.getElementById("homeButton").addEventListener("click", () => {
+const homeButton = document.getElementById("homeButton");
+homeButton.addEventListener("click", () => {
+  clearActiveMode();
+  changeActiveMode(homeButton);
   clearContent(main);
   loadHomePage(main);
 });
 
-document.getElementById("menuButton").addEventListener("click", () => {
+const menuButton = document.getElementById("menuButton");
+menuButton.addEventListener("click", () => {
+  clearActiveMode();
+  changeActiveMode(menuButton);
   clearContent(main);
   loadMenuPage(main);
 });
 
-document.getElementById("contactButton").addEventListener("click", () => {
+const contactButton = document.getElementById("contactButton");
+contactButton.addEventListener("click", () => {
+  clearActiveMode();
+  changeActiveMode(contactButton);
   clearContent(main);
   loadContactPage(main);
 });
 
 function clearContent(main) {
   main.innerHTML = "";
+}
+
+function changeActiveMode(btn) {
+  btn.classList.add("active");
+}
+
+function clearActiveMode() {
+  homeButton.classList.remove("active");
+  menuButton.classList.remove("active");
+  contactButton.classList.remove("active");
 }
